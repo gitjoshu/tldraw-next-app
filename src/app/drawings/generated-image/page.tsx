baf707644cce8fe5a828c8f59d5caf1222d52c48
joshudev <joshua.munoz.barbero@gmail.com>
2025-04-13 18:18:09 +0200

    fix(generated-image): fix localStorage client

--- src/app/drawings/generated-image/page.tsx
+++ src/app/drawings/generated-image/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function GeneratedImagePage() {
-  const generatedImage = localStorage.getItem("generatedImage");
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
 
-  if (!generatedImage) {
+  useEffect(() => {
+    const generatedImage = localStorage.getItem("generatedImage");
+    setImageUrl(generatedImage);
+  }, []);
+
+  if (!imageUrl) {
     return <h2 className="text-2xl font-bold">No generated image found</h2>;
   }
 
@@ -13,7 +20,7 @@ export default function GeneratedImagePage() {
     <>
       <h2 className="text-2xl font-bold mb-4">Generated Image</h2>
       <Image
-        src={generatedImage}
+        src={imageUrl}
         alt="Generated Image"
         width={100}
         height={100}
